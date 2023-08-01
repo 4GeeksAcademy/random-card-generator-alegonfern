7e8fbc71b8c776193adae19dc4666bb96061dff7
Alexis Gonzalez <94706121+alegonfern@users.noreply.github.com>
2023-08-01 04:34:40 +0000

    act

--- src/app.js
+++ src/app.js
@@ -9,3 +9,39 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const suits = ["Corazones", "Picas", "Clubes", "Diamantes"];
+const values = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Jota",
+  "Reina",
+  "Rey",
+  "As"
+];
+
+function getRandomCard() {
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  return `${randomValue} de ${randomSuit}`;
+}
+
+function updateCard() {
+  const cardElement = document.getElementById("card");
+  cardElement.textContent = getRandomCard();
+}
+
+// Actualizar la carta aleatoriamente al cargar la página
+updateCard();
+
+// Actualizar la carta aleatoriamente cada vez que el sitio web se actualice
+window.addEventListener("load", function() {
+  setInterval(updateCard, 5000); // Actualizar cada 5 segundos (puedes ajustar el tiempo según tus necesidades)
+});
