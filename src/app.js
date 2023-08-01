88f9c6f49b95a78ba5641d2af2996cb457450089
Alexis Gonzalez <94706121+alegonfern@users.noreply.github.com>
2023-08-01 04:54:58 +0000

    parcial

--- src/app.js
+++ src/app.js
@@ -1,16 +1,7 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-
-const suits = ["Corazones", "Picas", "Clubes", "Diamantes"];
+import "./styles.css";
+const suits = ["&hearts;", "&spades;", "&clubs;", "&diams;"];
 const values = [
   "2",
   "3",
@@ -21,27 +12,25 @@ const values = [
   "8",
   "9",
   "10",
-  "Jota",
-  "Reina",
-  "Rey",
-  "As"
+  "J",
+  "Q",
+  "K",
+  "A"
 ];
 
 function getRandomCard() {
   const randomSuit = suits[Math.floor(Math.random() * suits.length)];
   const randomValue = values[Math.floor(Math.random() * values.length)];
-  return `${randomValue} de ${randomSuit}`;
+  return `${randomValue} ${randomSuit}`;
 }
 
 function updateCard() {
   const cardElement = document.getElementById("card");
-  cardElement.textContent = getRandomCard();
+  cardElement.innerHTML = getRandomCard();
 }
 
 // Actualizar la carta aleatoriamente al cargar la página
 updateCard();
 
 // Actualizar la carta aleatoriamente cada vez que el sitio web se actualice
-window.addEventListener("load", function() {
-  setInterval(updateCard, 5000); // Actualizar cada 5 segundos (puedes ajustar el tiempo según tus necesidades)
-});
+window.addEventListener("load", function() {});
